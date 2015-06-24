db898264369d14502f4d30f2981862082240a882
Daz Ahern <darren.ahern@gmail.com>
2015-06-24 14:33:21 +0100

    generate comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "posts#index"
   resources :posts do
-  	resources :comments
+  	resources :comments, only: [:new, :create, :destroy]
 	end
 # This generates the seven RESTful resources
 # Get etc
